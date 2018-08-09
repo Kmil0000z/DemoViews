9753972bbc95f5ee3045d4fad98f1c7e3e3d21b2
Kmil0000z <41449113+Kmil0000z@users.noreply.github.com>
2018-08-09 14:13:36 -0500

    Vistas

---
+++ DemoViews/DemoViews/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoViews
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            timPickInicial.Time = new TimeSpan(17,0,0);
+        }
+
+        async void Handle_Navigated(object sender, Xamarin.Forms.WebNavigatedEventArgs e)
+        {
+            await DisplayAlert("¡Perfecto!","La página web ha cargado","Aceptar");
+        }
+    }
+}
