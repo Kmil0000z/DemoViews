35e4beebed86aeba775d911a3b562834fcd345d2
Kmil0000z <41449113+Kmil0000z@users.noreply.github.com>
2018-08-23 17:46:55 -0500

    Final

--- DemoStackLayout/DemoStackLayout/App.xaml.cs
+++ DemoStackLayout/DemoStackLayout/App.xaml.cs
@@ -11,7 +11,7 @@ namespace DemoStackLayout
         {
             InitializeComponent();
 
-            MainPage = new DemoAbsolutLayout();
+            MainPage = new Calculadora();
         }
 
         protected override void OnStart()
